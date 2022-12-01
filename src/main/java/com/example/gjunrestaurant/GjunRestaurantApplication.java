21aaa2e9d55be63da8afeb382e42f207efd2714d
AndyTsai <andy829173@gmail.com>
2022-12-01 23:12:03 +0800

    First commit
    Basic configuration

---
+++ src/main/java/com/example/gjunrestaurant/GjunRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.example.gjunrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjunRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GjunRestaurantApplication.class, args);
+    }
+
+}
