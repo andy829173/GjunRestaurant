ccbec56b3d16a7a4333e77acbcb4e2e39f1817e0
AndyTsai <andy829173@gmail.com>
2022-12-20 12:18:23 +0800

    feat: add two querys in OrderDAO
    queryFindAll()、deleteByOrderID(String orderID)

--- src/main/java/com/example/gjunrestaurant/dao/OrderDao.java
+++ src/main/java/com/example/gjunrestaurant/dao/OrderDao.java
@@ -1,7 +1,18 @@
 package com.example.gjunrestaurant.dao;
 
 import com.example.gjunrestaurant.entity.Order;
+import com.example.gjunrestaurant.entity.OrderItem;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OrderDao extends CrudRepository<Order, Integer> {
+
+    // 自定義SQL查詢
+    @Query(value = "SELECT * FROM order", nativeQuery = true)
+    List<Order> queryFindAll();
+
+    @Query(value = "DELETE FROM order WHERE id = ?1", nativeQuery = true)
+    OrderItem deleteByOrderID(String orderID);
 }
