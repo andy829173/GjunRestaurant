c623a26199c67112a41e93626e8d681d6aee0fbe
AndyTsai <andy829173@gmail.com>
2022-12-20 12:47:15 +0800

    fix: add empty constructor
    to create orderID

--- src/main/java/com/example/gjunrestaurant/entity/Order.java
+++ src/main/java/com/example/gjunrestaurant/entity/Order.java
@@ -6,16 +6,18 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Entity
 @Table(name = "`order`")
 @Getter
 @Setter
-@NoArgsConstructor
 @ToString
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Integer ID;
+    @Column(name = "id")
+    String ID;
     @Column(name = "date")
     String date;
     @Column(name = "time")
@@ -25,6 +27,26 @@ public class Order {
     @Column(name = "paid")
     Boolean paid;
 
+    public Order() {
+        Date dateTime = new Date();
+        // 處理ID格式
+        SimpleDateFormat idDate = new SimpleDateFormat("yyyyMMdd");
+        SimpleDateFormat idTime = new SimpleDateFormat("HHmmss");
+        String dateForID = idDate.format(dateTime);
+        String timeForID = idTime.format(dateTime);
+        this.ID = dateForID + timeForID;
+
+        // 處理Date + Time
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+        String date = dateFormat.format(dateTime);
+        String time = timeFormat.format(dateTime);
+        this.date = date;
+        this.time = time;
+        this.totalPrice = 0;
+        this.paid = false;
+    }
+
     public Order(String date, String time, Integer totalPrice) {
         this.ID = ID;
         this.date = date;
@@ -32,4 +54,5 @@ public class Order {
         this.totalPrice = totalPrice;
         this.paid = false;
     }
+
 }
