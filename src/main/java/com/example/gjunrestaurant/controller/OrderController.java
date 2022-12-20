58182cbae776607026d20ea80978d369d38fc7dc
AndyTsai <andy829173@gmail.com>
2022-12-20 12:49:12 +0800

    refactor: OrderController

--- src/main/java/com/example/gjunrestaurant/controller/OrderController.java
+++ src/main/java/com/example/gjunrestaurant/controller/OrderController.java
@@ -1,70 +1,54 @@
 package com.example.gjunrestaurant.controller;
 
 import com.example.gjunrestaurant.entity.Order;
+import com.example.gjunrestaurant.entity.OrderItem;
 import com.example.gjunrestaurant.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/order")
 public class OrderController {
 
     @Autowired
     OrderService orderService;
 
-    @PostMapping("/create")
-    public String createOrder(@RequestBody Order order) {
-        orderService.createOrder(order);
-        return "Create success";
+    @PostMapping("/order")
+    public String createOrder(@RequestBody List<OrderItem> orderItemList) {
+        return orderService.createOrder(orderItemList);
     }
 
-    @GetMapping("/read")
-    public Iterable<Order> getOrder() {
-        Iterable<Order> orderList = orderService.getOrder();
-        return orderList;
+    @GetMapping("/order")
+    public List<Order> getOrder() {
+        return orderService.getOrders();
     }
 
-    @PostMapping("/paid")
-    public String updateOrder(Integer id) {
+    @PutMapping("/order")
+    public String updateOrder(String orderID, List<OrderItem> orderItemList) {
+        return orderService.updateOrderItem(orderID, orderItemList);
+    }
+
+    @DeleteMapping("/order")
+    public String deleteOrder(String orderID) {
+        return orderService.deleteOrder(orderID);
+    }
+
+    @PatchMapping("/order/paid")
+    public String toPaid(String orderID) {
+        String msg;
         try {
-            orderService.toPay(id);
-            return "Paid!!";
+            msg = orderService.toPay(orderID);
         } catch (Exception e) {
-            return "Failed";
+            msg = "Pay Failed";
         }
-    }
-
-    @PostMapping("/updatePrice")
-    public Order updatePrice(Integer id, Integer totalPrice) {
-        orderService.updatePrice(id, totalPrice);
-        return orderService.findById(id);
+        return msg;
     }
 
     @GetMapping("/test")
-    public String insertData() {
-        List<Order> data = List.of(new Order("2022/12/8", "18:00", 1380),
-                new Order("2022/12/1", "14:00", 980),
-                new Order("2022/12/2", "17:30", 1080),
-                new Order("2022/12/3", "18:45", 1880),
-                new Order("2022/12/4", "19:30", 2160),
-                new Order("2022/12/5", "12:30", 4580),
-                new Order("2022/12/6", "11:45", 2330),
-                new Order("2022/12/7", "11:00", 1170),
-                new Order("2022/12/9", "12:00", 1580),
-                new Order("2022/12/10", "14:00", 1673),
-                new Order("2022/12/11", "17:00", 1490),
-                new Order("2022/12/12", "17:45", 2180),
-                new Order("2022/12/13", "18:00", 2960)
-        );
-        try {
-            for (Order order : data) {
-                orderService.createOrder(order);
-            }
-            return "Success";
-        } catch (Exception e) {
-            return "Failed";
-        }
+    public String justTest(Model model) {
+        model.addAttribute("msg", "test");
+        return "orderPage";
     }
 }
