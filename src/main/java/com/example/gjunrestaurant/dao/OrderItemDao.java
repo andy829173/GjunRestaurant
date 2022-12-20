d4ebf26a0f9bf9129a7b4f5803fc426c8e110f78
AndyTsai <andy829173@gmail.com>
2022-12-20 12:19:08 +0800

    feat: add query in OrderItemDAO
    deleteByProductID(Integer productID)

--- src/main/java/com/example/gjunrestaurant/dao/OrderItemDao.java
+++ src/main/java/com/example/gjunrestaurant/dao/OrderItemDao.java
@@ -9,6 +9,9 @@ import java.util.List;
 public interface OrderItemDao extends CrudRepository<OrderItem, Integer> {
 
     // 自定義SQL查詢
-    @Query(value = "select * from order_item where orderid = ?1", nativeQuery = true)
-    List<OrderItem> queryByOrderID(Integer orderID);
+    @Query(value = "SELECT * FROM order_item WHERE orderid = ?1", nativeQuery = true)
+    List<OrderItem> queryByOrderID(String orderID);
+
+    @Query(value = "DELETE FROM order_item WHERE productid = ?1", nativeQuery = true)
+    OrderItem deleteByProductID(Integer productID);
 }
