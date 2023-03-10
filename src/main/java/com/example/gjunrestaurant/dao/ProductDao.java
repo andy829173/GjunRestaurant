012e27e5cbcd0fd99ea194ebb8fa7fe0c7676ce4
AndyTsai <andy829173@gmail.com>
2023-03-10 16:25:34 +0800

    perf: CrudRepository > JpaRepository

--- src/main/java/com/example/gjunrestaurant/dao/ProductDao.java
+++ src/main/java/com/example/gjunrestaurant/dao/ProductDao.java
@@ -1,7 +1,7 @@
 package com.example.gjunrestaurant.dao;
 
 import com.example.gjunrestaurant.entity.Product;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductDao extends CrudRepository<Product, Integer> {
+public interface ProductDao extends JpaRepository<Product, Integer> {
 }
